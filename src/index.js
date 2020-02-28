a26f2aa5af329ca62b3f3219f4b337e312f5d79d
DenisRebenock <denmaloyreb@mail.ru>
2020-02-28 20:09:00 +0300

    refactor: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str1 = Math.abs(n);
+    let str = String(str1);
+    let mas = str.split("");
+    let rev = mas.reverse().join("");
+    regits = Number(rev);
+    return(res);
+
 }
